45d4d9719bac7777d27f3c99c7ea920c11b3dc40
Harvy Mosquera <hmosquera@hellobuild.co>
2021-10-08 01:01:24 -0500

    fix: minor validation fix

--- src/views/Panel.jsx
+++ src/views/Panel.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
-
+import { getUserData } from '../utils/services';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
 import Track from './Track';
 import Favorites from './Favorites';
 function Panel() {
 	const { path } = useRouteMatch()
+	let [user, setUser] = useState({})
+
+    useEffect(() => {
+        setUser(getUserData())
+    }, [])
+
+	useEffect(() => {
+		if(user.code === 3001) {
+			window.location.href = '/signin'
+		}
+	}, [user])
 	return (
 		<Switch>
 			<Route exact path={`${path}/home`} >
